37cba4399bf1b22e23a165c1ba1dbe835f950065
Matthew Johnson <mjj29@ceres.matthew.ath.cx>
2021-01-31 17:38:56 +0000

    Fix upgrading from old schema and add test

--- tests/Upgrade/run.py
+++ tests/Upgrade/run.py
@@ -7,17 +7,12 @@ class PySysTest(BaseTest):
 	def execute(self):
 
 		# start initial correlator
-		pathpath = os.path.join(self.output,'pathdb')
+		self.copy(self.input+'/storedb', self.output)
 		storepath = os.path.join(self.output,'storedb')
-		corr = CorrelatorHelper(self, name='correlator-init')
-		corr.start(logfile='correlator-init.log')
-		self.runTest(corr, f'TestRun("{pathpath}","{storepath}",true)')
+		corr = CorrelatorHelper(self, name='correlator')
+		corr.start(logfile='correlator.log', arguments=['--applicationLogLevel', 'DEBUG'])
+		self.runTest(corr, f'TestRun("{storepath}")')
 		corr.shutdown()
-		corr = CorrelatorHelper(self, name='correlator-recovery')
-		corr.start(logfile='correlator-recovery.log')
-		self.runTest(corr, f'TestRun("{pathpath}","{storepath}",false)')
-		corr.shutdown()
-
 
 	def runTest(self, corr, event):
 		corr.injectEPL(filenames=['ConnectivityPluginsControl.mon', 'ConnectivityPlugins.mon', 'AnyExtractor.mon', 'data_storage/MemoryStore.mon', 'HTTPClientEvents.mon', 'TimeFormatEvents.mon'], filedir=self.project.APAMA_HOME+'/monitors')
@@ -31,10 +26,5 @@ class PySysTest(BaseTest):
 		corr.flush(count=2)
 
 	def validate(self):
-		for i in [ "fromStore", "fromPath", "inMemory" ]:
-			self.assertGrep('correlator-init.log', expr='Starting tests for '+i)
-			self.assertGrep('correlator-init.log', expr='Completed tests for '+i)
-			self.assertGrep('correlator-recovery.log', expr='Starting tests for '+i)
-			self.assertGrep('correlator-recovery.log', expr='Completed tests for '+i)
-		self.assertGrep('correlator-init.log', expr=' ERROR ', contains=False)
-		self.assertGrep('correlator-recovery.log', expr=' ERROR ', contains=False)
+		self.assertGrep('correlator.log', expr=' ERROR ', contains=False)
+		self.assertGrep('correlator.log', expr='Completed tests')
